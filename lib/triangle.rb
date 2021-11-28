145efc9f5ec1894ee640e3518ffdf70995d14d19
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-28 16:30:38 -0500

    commit tree 99519fe81b33cacff948bde46034d1e5f601b199

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sides = [@side1, @side2, @side3]
+    sides.each do |given_side|
+      if given_side <= 0
+        begin
+          raise TriangleError
+        end
+      end
+    end
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 ||
+         @side1 + @side3 <= @side2
+      begin
+        raise TriangleError
+      end
+    end
+    sidestally = sides.tally
+    if sidestally.values.length === 1
+      :equilateral
+    elsif sidestally.values.length === 2
+      :isosceles
+    elsif sidestally.values.length === 3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def too_small
+      'This triangle has no size!'
+    end
+  end
 end
